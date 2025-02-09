7d860523644171a0e95c3a40a5ae6febfde3dd1e
coderpeti <koderpeter13@gmail.com>
2025-02-09 15:30:58 +0100

    Change Database to PostgresSql2

--- airport_app/airport_app/settings.py
+++ airport_app/airport_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'airport_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Airport_database',
+        'USER': 'peter_coder',
+        'PASSWORD': '@HarmonHealth',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
@@ -121,7 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Ez segít, hogy a statikus fájlok a megfelelő mappából legyenek betöltve.
+    os.path.join(BASE_DIR, 'users/static'),  
+    os.path.join(BASE_DIR, 'workers/static'), 
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
